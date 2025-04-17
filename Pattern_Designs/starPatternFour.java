24f92265f988bb87b1a01fb1409f9755175433db
Sai-Swarup-dev <saiswaruppatnaik14@gmail.com>
2025-04-17 21:04:31 +0530

    Lower Triangle Pattern - Interview Question

---
+++ Pattern_Designs/starPatternFour.java
@@ -0,0 +1,28 @@
+package InterviewQuestion.Pattern_Designs;
+/*
+ * 
+Output:
+=========
+ * * * * 
+  * * * 
+   * * 
+    * 
+ */
+public class starPatternFour {
+    public static void main(String[] args) {
+        int sp = 0;
+        int st = 4;
+        for(int i=1; i<=4; i++){
+            for(int k=0; k<=sp; k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=st; j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+            sp++;
+            st--;
+        }
+    }
+}
