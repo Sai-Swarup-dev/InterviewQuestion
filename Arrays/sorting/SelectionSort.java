6421836ec9c47bbe6c361751a59446ec4a11a253
Sai-Swarup-dev <saiswaruppatnaik14@gmail.com>
2025-04-18 13:53:20 +0530

    Selection Sorting - Interview Question

---
+++ Arrays/sorting/SelectionSort.java
@@ -0,0 +1,42 @@
+package InterviewQuestion.Arrays.sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+        System.out.println("Original array:");
+        printArray(array);
+
+        selectionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Selection Sort method
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // Update minIndex if a smaller element is found
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
